819083cf10e5d53dac5dac641504d2834b51ebee
sidhanathdon <sid55boss@gmail.com>
2023-07-02 18:50:16 +0530

    changes

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { Toaster } from "react-hot-toast";
+import { themeSettings } from "./theme";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Summary from "./pages/Summary";
+import Paragraph from "./pages/Paragraph";
+import ChatBot from "./pages/ChatBot";
+import JsConverter from "./pages/JsConverter";
+import ScifiImage from "./pages/ScifiImage";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings()), []);
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Toaster />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/summary" element={<Summary />} />
+          <Route path="/paragraph" element={<Paragraph />} />
+          <Route path="/chatbot" element={<ChatBot />} />
+          <Route path="/js-converter" element={<JsConverter />} />
+          <Route path="/scifi-image" element={<ScifiImage />} />
+        </Routes>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const errorHandler = require("./middelwares/errorMiddleware");
+
+
+//dotenv
+dotenv.config();
+
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 8080;
+
+//API routes
+app.use("/api/v1/openai", require("./routes/openaiRoutes"));
+
+//listen server
+app.listen(PORT, () => {
+  console.log(    `Server Running in ${process.env.DEV_MODE} mode on port no ${PORT}`.bgCyan
+      .white
+  );
+});
